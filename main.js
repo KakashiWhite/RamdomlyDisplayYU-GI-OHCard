c05608bad2533e8ff04a0721c7d2fa2b811ae36d
KakashiWhite <kakashi555913333@yahoo.co.jp>
2021-01-22 00:46:24 +0900

    cid=6561, 15932, 15936を除外項目として追加

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $('.cardgame-deck').on('click', function(){
     const cid_min = 4007;  // cidの（おそらく）最小値。青眼の白龍のcid。
     const cid_max = 16041;  // 2021/1/21時点でのcidの（おそらく）最大値。福悲喜のcid。
-    const exclude_ids = [];
+    const exclude_ids = [6561, 15932, 15936];
     let cid_random;
     do{
         // ランダムなcidを生成。
