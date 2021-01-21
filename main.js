941c455505b86db75808d181a79637badb2bdede
KakashiWhite <kakashi555913333@yahoo.co.jp>
2021-01-22 00:45:18 +0900

    特定のcidの除外処理を追加。

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 $('.cardgame-deck').on('click', function(){
     const cid_min = 4007;  // cidの（おそらく）最小値。青眼の白龍のcid。
     const cid_max = 16041;  // 2021/1/21時点でのcidの（おそらく）最大値。福悲喜のcid。
-    const cid_random =  Math.floor( Math.random() * ((cid_max + 1) - cid_min)) + cid_min;
+    const exclude_ids = [];
+    let cid_random;
+    do{
+        // ランダムなcidを生成。
+        cid_random =  Math.floor( Math.random() * ((cid_max + 1) - cid_min)) + cid_min;
+    }while(exclude_ids.some( num => num === cid_random));   // 情報がないcidだった場合は再算出。
+    
     const base_url = "https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=2&cid="
     let window_A = window.open(base_url + cid_random);
 });
