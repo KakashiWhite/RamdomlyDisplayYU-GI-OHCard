5141400c4a6cb4cb87ac1c05438d09ad2211cafa
KakashiWhite <kakashi555913333@yahoo.co.jp>
2021-01-22 14:26:06 +0900

    Update main.js

--- main.js
+++ main.js
@@ -42,9 +42,12 @@ $('.cardgame-deck').on('click', function(){
 
         let sleep_time = 2*1000;    // 待機時間
         sleep(sleep_time, function() {
-            $(".spinner-border").remove();
-            $('#fullOverlay').hide();
-            let window_A = window.open(result_url);
+            $.when(
+                $(".spinner-border").remove(),
+                $('#fullOverlay').hide()
+            ).done(function() {
+                let window_A = window.open(result_url)
+            })
         });
     });
 });
