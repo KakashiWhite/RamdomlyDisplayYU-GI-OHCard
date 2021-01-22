7b3afd2901e8f9766c6cd24d069b8c10e243e310
KakashiWhite <kakashi555913333@yahoo.co.jp>
2021-01-22 14:43:49 +0900

    画面ロック削除

--- main.js
+++ main.js
@@ -19,32 +19,27 @@ document.addEventListener('DOMContentLoaded', function () {
 }, false);
 
 $('.cardgame-deck').on('click', function(){
-    $.when(
-        // $('#fullOverlay').show(),    // 画面をロックする。
-        // $("#spinner").prepend('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>')
-	).done(function() {
-        const cid_min = 4007;  // cidの（おそらく）最小値。青眼の白龍のcid。
-        const cid_max = 16041;  // 2021/1/21時点でのcidの（おそらく）最大値。福悲喜のcid。
-        
-        let cid_random;
-        let counter=0;  // do-while文の実行回数。
-        const counter_max=10; // do-while文の最大実行回数。膨大なループを防ぐ。
-        do{
-            cid_random =  Math.floor( Math.random() * ((cid_max + 1) - cid_min)) + cid_min; // ランダムなcidを生成。
-            counter ++;
-        }while(exclude_ids.some( num => num === cid_random) || counter < counter_max);   // 情報がないcidだった場合は再算出。
-        
-        result_url = base_url + cid_random;
-        
-        function sleep(ms, func) {
-            setTimeout(func, ms);
-        };
+    const cid_min = 4007;  // cidの（おそらく）最小値。青眼の白龍のcid。
+    const cid_max = 16041;  // 2021/1/21時点でのcidの（おそらく）最大値。福悲喜のcid。
+    
+    let cid_random;
+    let counter=0;  // do-while文の実行回数。
+    const counter_max=10; // do-while文の最大実行回数。膨大なループを防ぐ。
+    do{
+        cid_random =  Math.floor( Math.random() * ((cid_max + 1) - cid_min)) + cid_min; // ランダムなcidを生成。
+        counter ++;
+    }while(exclude_ids.some( num => num === cid_random) || counter < counter_max);   // 情報がないcidだった場合は再算出。
+    
+    result_url = base_url + cid_random;
+    
+    function sleep(ms, func) {
+        setTimeout(func, ms);
+    };
 
-        let sleep_time = 2*1000;    // 待機時間
-        sleep(sleep_time, function() {
-            // $(".spinner-border").remove();
-            // $('#fullOverlay').hide();
-            let window_A = window.open(result_url);
-        });
+    let sleep_time = 2*1000;    // 待機時間
+    sleep(sleep_time, function() {
+        // $(".spinner-border").remove();
+        // $('#fullOverlay').hide();
+        let window_A = window.open(result_url);
     });
 });
