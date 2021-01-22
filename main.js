31eefd860909c0a87e9399ff1a72ebef859606e4
KakashiWhite <kakashi555913333@yahoo.co.jp>
2021-01-22 13:39:36 +0900

    画面のロック機能の追加

--- main.js
+++ main.js
@@ -16,23 +16,30 @@ document.addEventListener('DOMContentLoaded', function () {
 }, false);
 
 $('.cardgame-deck').on('click', function(){
-    const cid_min = 4007;  // cidの（おそらく）最小値。青眼の白龍のcid。
-    const cid_max = 16041;  // 2021/1/21時点でのcidの（おそらく）最大値。福悲喜のcid。
-    
-    let cid_random;
-    do{
-        // ランダムなcidを生成。
-        cid_random =  Math.floor( Math.random() * ((cid_max + 1) - cid_min)) + cid_min;
-    }while(exclude_ids.some( num => num === cid_random));   // 情報がないcidだった場合は再算出。
-    
-    const base_url = "https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=2&cid="
-    
-    function sleep(ms, func) {
-        setTimeout(func, ms);
-    };
+    $.when(
+        $('#fullOverlay').show(),    // 画面をロック
+        $("#spinner").prepend('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>')
+	).done(function() {
+        const cid_min = 4007;  // cidの（おそらく）最小値。青眼の白龍のcid。
+        const cid_max = 16041;  // 2021/1/21時点でのcidの（おそらく）最大値。福悲喜のcid。
+        
+        let cid_random;
+        do{
+            // ランダムなcidを生成。
+            cid_random =  Math.floor( Math.random() * ((cid_max + 1) - cid_min)) + cid_min;
+        }while(exclude_ids.some( num => num === cid_random));   // 情報がないcidだった場合は再算出。
+        
+        const base_url = "https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=2&cid="
+        
 
-    sleep(1*1000, function() {
-        let window_A = window.open(base_url + cid_random);
+        function sleep(ms, func) {
+            setTimeout(func, ms);
+        };
+
+        sleep(2*1000, function() {
+            let window_A = window.open(base_url + cid_random);
+            $(".spinner-border").remove();
+            $('#fullOverlay').hide();
+        });
     });
-
 });
